40ddb2edd32f40b9b02b378580faeb0b09fd7250
Siriwardena <Sumeda.Siriwardena@fao.org>
2025-03-28 15:09:16 +0100

    update

--- R/visualize_data_production.R
+++ R/visualize_data_production.R
@@ -2,9 +2,7 @@
 
 #data should be in wide format for this function. 
 
-visualize_data_production=function(data,END_YEAR, session){
-  
-  
+visualize_data_production=function(data,input ,session){
   
   flagcols <- grep("^Flag", names(data), value = TRUE)
   yearcols <- grep("^[[:digit:]]{4}$", names(data), value = TRUE)
@@ -15,46 +13,33 @@ visualize_data_production=function(data,END_YEAR, session){
   # Convert types
   data[, (flagcols) := lapply(.SD, as.character), .SDcols = flagcols]
   data[, (yearcols) := lapply(.SD, as.numeric), .SDcols = yearcols]
-  
-  
-  
-  # yearErrors(END_YEAR, minyear, maxyear,session)
-  
-  desired_yearset <- minyear:END_YEAR
-  
-  final_data <- if(maxyear == END_YEAR){
+  desired_yearset <- minyear:as.numeric(input$endyear)
+  final_data <- if(maxyear == input$endyear){
     
     data[]
     
-  } else if(maxyear > END_YEAR){
-    
+  } else if(maxyear > input$endyear){
     tempdata <- copy(data)
-    delyears <- as.character((as.numeric(END_YEAR) + 1):maxyear)
+    delyears <- as.character((as.numeric(input$endyear) + 1):maxyear)
     delflags <- paste("Flag", delyears)  
-    
     tempdata[, c(delyears, delflags) := NULL]
     tempdata[]
     
-  } else if(maxyear < END_YEAR){
+  } else if(maxyear < input$endyear){
     
     tempdata <- copy(data)
     # Get names for column ordering later
     prevnames <- names(data)
-    
-    addyears <- as.character(END_YEAR)
+    addyears <- as.character(input$endyear)
     addflags <- paste("Flag", addyears)  
     
     #order columns year then flag
     addorder <- as.vector(rbind(addyears, addflags))
-    
     tempdata[, (addyears) := NA_real_]
     tempdata[, (addflags) := NA_character_]
-    
     setcolorder(tempdata, c(prevnames, addorder))
-    
     tempdata[]
-    
-  }
+}
   
   return(final_data)
   
