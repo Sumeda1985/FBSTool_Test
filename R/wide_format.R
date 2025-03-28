4ccbfa0edd3d07886755145be18c8e47a347eff5
Siriwardena <Sumeda.Siriwardena@fao.org>
2025-03-28 16:38:04 +0100

    update = add_order

--- R/wide_format.R
+++ R/wide_format.R
@@ -3,15 +3,16 @@
 wide_format=function(data){
 data =  dcast.data.table(data, CPCCode+Commodity+ElementCode+Element ~ Year,
                            value.var = c("Value","Flag"))
-names(data)[grep("^Flag",names(data))] <- gsub("_"," ",grep("^Flag", names(data), value=TRUE))
-names(data)[grep("^Value",names(data))] <- gsub("^.*?_","", grep("^Value", names(data), value=TRUE))
-addorder <- as.vector(rbind(yearcols_new, flagcols_new))
-setcolorder(data,
-              c("CPCCode", "Commodity", "ElementCode", "Element", addorder))
 data[, (names(data)[grep("^Flag",names(data))]) := lapply(.SD, as.character), .SDcols = 
        (names(data)[grep("^Flag",names(data))])]
 data[, (names(data)[grep("^Value",names(data))]):= lapply(.SD, as.numeric), 
      .SDcols = (names(data)[grep("^Value",names(data))])]
+addorder <- as.vector(rbind(names(data)[grep("^Value",names(data))],
+                            names(data)[grep("^Flag",names(data))] ))
+setcolorder(data,
+            c("CPCCode", "Commodity", "ElementCode", "Element", addorder))
+names(data)[grep("^Flag",names(data))] <- gsub("_"," ",grep("^Flag", names(data), value=TRUE))
+names(data)[grep("^Value",names(data))] <- gsub("^.*?_","", grep("^Value", names(data), value=TRUE))
 data[order(CPCCode)]
-  return(data)
+return(data)
 }
